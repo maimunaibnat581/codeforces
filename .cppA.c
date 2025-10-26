2738a6bc6b34db4414fe2c130b120897ea03d4b0
maimunaibnat581 <munayuqisong@gmail.com>
2025-10-26 20:32:30 +0600

    Add files via upload

---
+++ .cppA.c
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    cin >> name;              // Read the name as a string
+    cout << "Hello, " << name << endl;
+    return 0;
+}
